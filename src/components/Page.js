fa4d6dc183d95a049b8fd16005ea1df1498fee99
Viraj Patidar <vrj381@gmail.com>
2021-08-05 20:00:23 +0530

    form completed

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,9 +1,11 @@
 import Header from "./Header";
+import Form from "./Form";
 
 const Page = () => {
     return (
         <>
             <Header />
+            <Form submitSearch={true} />
             <h1>Page</h1>
         </>
     );
